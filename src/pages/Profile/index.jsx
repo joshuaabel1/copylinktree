6a42ace321beeb449744290f6fe84b4b8888274a
Perez Alan <diegoalanperezz@gmail.com>
2021-08-17 21:23:51 -0300

    fix styles

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,7 @@
-import Avatar from "../../components/avatar/avatar";
-import "../../components/avatar/style.css";
-
 export default function ProfilePage() {
   return (
     <>
       <h1>Profile Page</h1>
-      <Avatar />
     </>
   );
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,11 +1,15 @@
 import { useParams } from 'react-router-dom';
-
-export default function UserPage(props){
-    const {user} = useParams();
+import Avatar from "../../components/avatar/avatar.jsx";
+export default function UserPage(props) {
+    const { user } = useParams();
 
     // TODO: Si el usuario no existe, redireccionar a la pagina de not found
-    
+
     return (
-        <h1>UserPage</h1>
+        <>
+            <h1>UserPage</h1>
+            <Avatar />
+
+        </>
     );
 }
