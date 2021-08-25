d7e992d0ee421f48a193d8544203cc6f38393453
SebastianMacaya <contacto@sebastianmacaya.com>
2021-08-24 21:33:49 -0300

    agrego 2 componentes

---
+++ src/components/newLinkButton/button.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+const NewLinkButton = () => {
+  return (
+    <>
+      <div className="addLinkContainer">
+        <button className="addLinkButton">Add New Link</button>
+      </div>
+    </>
+  );
+};
+
+export default NewLinkButton;
